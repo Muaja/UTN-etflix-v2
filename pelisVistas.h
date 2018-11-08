305bc0091ee05842cf5843f34e7099bb08932c76
Muaja <rodrii.fan@gmail.com>
2018-11-08 20:44:38 -0300

    Include guards

--- pelisVistas.h
+++ pelisVistas.h
@@ -1,11 +1,11 @@
+#ifndef PELISVISTAS_H_INCLUDED
+#define PELISVISTAS_H_INCLUDED
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "listaPeliculas.h"
 #include "ADLusuarios.h"
 #include "Estructuras.h"
-#ifndef PELISVISTAS_H_INCLUDED
-#define PELISVISTAS_H_INCLUDED
 
 void pasarArchivoPelisVistaToADL(stPelisVistas pelisVistas[], int val, nodoListaPelicula * listaP, stCelda user[],int cantUser);
 int cargarADLpelisVistas(stPelisVistas pelisVistas[], int val, stPelisVistas datos, nodoListaPelicula * listaP, stCelda user[],int cantUser);
