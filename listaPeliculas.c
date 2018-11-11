8082e57b901049956256b8bbea66ba0720122d66
Muaja <rodrii.fan@gmail.com>
2018-11-11 10:36:58 -0300

    Quitadas funciones de creacion y persistencia

--- listaPeliculas.c
+++ listaPeliculas.c
@@ -5,28 +5,28 @@ nodoListaPelicula *inicLista()
     return NULL;
 }
 
-int cantidadPeliculas(nodoListaPelicula * listaP)
+int cantidadPeliculas(nodoListaPelicula * lista)
 {
     int numero = 0;
-    if(listaP)
+    if(lista)
     {
-        numero = 1 + cantidadPeliculas(listaP->sig);
+        numero = 1 + cantidadPeliculas(lista->sig);
     }
     return numero;
 }
 
-int existePelicula(nodoListaPelicula * listaP, char nombre[])
+int existePelicula(nodoListaPelicula * lista, char nombre[])
 {
     int flag = 0;
-    if(listaP)
+    if(lista)
     {
-        if(!strcmpi(listaP->p.nombrePelicula, nombre))
+        if(!strcmpi(lista->p.nombrePelicula, nombre))
         {
-            flag = listaP->p.idPelicula;
+            flag = lista->p.idPelicula;
         }
         else
         {
-            flag = existePelicula(listaP->sig,nombre);
+            flag = existePelicula(lista->sig,nombre);
         }
     }
     return flag;
@@ -40,302 +40,76 @@ nodoListaPelicula * crearNodoListaPelicula(stPelicula nuevo)
     return aux;
 }
 
-stPelicula cargaPelicula()
+nodoListaPelicula * insertarPeliPpio(nodoListaPelicula * lista, nodoListaPelicula * nuevoNodo)
 {
-    stPelicula aux;
-
-    printf("\nIngrese el nombre de la nueva pelicula:\n");
-    fflush(stdin);
-    gets(aux.nombrePelicula);
-    while(!strlen(aux.nombrePelicula))
+    if(lista==NULL)
     {
-        printf("\nIngrese el nombre de la nueva pelicula (Error: El campo pelicula no puede estar vacio):\n");
-        fflush(stdin);
-        gets(aux.nombrePelicula);
-    }
-    while(existePelicula(listaP,aux.nombrePelicula))
-    {
-        printf("\nIngrese el nombre de la nueva pelicula (Error: La pelicula ya existe):\n");
-        fflush(stdin);
-        gets(aux.nombrePelicula);
-    }
-
-    printf("\nIngrese el nombre del director:\n");
-    fflush(stdin);
-    gets(aux.director);
-
-    printf("\nIngrese el genero de la pelicula:\n");
-    fflush(stdin);
-    gets(aux.genero);
-
-    printf("\nIngrese el pais de la pelicula:\n");
-    fflush(stdin);
-    gets(aux.pais);
-
-    printf("\nIngrese el anio en que se grabo la pelicula:\n");
-    fflush(stdin);
-    scanf("%i", &aux.anio);
-
-    printf("\nIngrese la valoracion inicial de la pelicula:\n");
-    fflush(stdin);
-    scanf("%i", &aux.valoracion);
-
-    //(0- si es ATP / 13: mayor de trece / 16: mayor de 16 / 18: mayor de 18;
-    printf("\nIngrese la clasificacion por edad de la pelicula:\n(0- ATP / 13- mayor de 13 / 16- mayor de 16 / 18- mayor de 18):\n");
-    fflush(stdin);
-    scanf("%i", &aux.pm);
-    while(aux.pm != 0 && aux.pm != 13 && aux.pm != 16 && aux.pm != 18)
-    {
-        printf("\nIngrese la clasificacion por edad de la pelicula\n(0- si es ATP / 13: mayor de trece / 16: mayor de 16 / 18: mayor de 18):\n");
-        fflush(stdin);
-        scanf("%i", &aux.pm);
-    }
-
-    printf("\nIngrese la URL de la pelicula: ");
-    fflush(stdin);
-    gets(aux.url);
-    while(!strlen(aux.url))
-    {
-        printf("\nIngrese la URL de la pelicula (Error: El campo URL no puede estar vacio): ");
-        fflush(stdin);
-        gets(aux.url);
-    }
-
-    aux.eliminado = 0;
-    aux.idPelicula = cantidadPeliculas(listaP)+1;
-    return aux;
-}
-
-void altaPelicula()
-{
-    stPelicula aux = cargaPelicula();
-
-    int opcion = 0;
-
-    encabezado("POSICION PELICULA","ADMINISTRADOR");
-    printf("\n1-Principio");
-    printf("\n2-Final");
-    printf("\n3-En orden por nombre");
-
-    printf("\n\n0-Volver atras");
-    printf("\n\nEsperando opcion: ");
-    scanf("%i", &opcion);
-
-    if(opcion == 0)
-    {
-        menuAdministrarPeliculas();
+        lista=nuevoNodo;
     }
     else
     {
-        nodoListaPelicula * nuevo = crearNodoListaPelicula(aux);
-
-        switch(opcion)
-        {
-            case 1:
-            {
-                listaP = insertarPeliPpio(listaP,nuevo);
-                break;
-            }
-            case 2:
-            {
-                listaP = insertarPeliFinal(listaP,nuevo);
-                break;
-            }
-            case 3:
-            {
-                listaP = insertarPeliOrden(listaP,nuevo);
-                break;
-            }
-        }
-        listaPelisAArchivo(ARCHIVO_PELICULAS, listaP);
+        nuevoNodo->sig=lista;
+        lista=nuevoNodo;
     }
-
-    printf("\nLa pelicula se guardo correctamente.\n\n");
-    system("pause");
+    return lista;
 }
 
-void modificarPelicula(int idPelicula)
+nodoListaPelicula * insertarPeliFinal(nodoListaPelicula * lista, nodoListaPelicula * nuevoNodo)
 {
-    nodoListaPelicula * aux = buscarListaPeliID(idPelicula,listaP);
-
-    int opcion = 0;
-
-    encabezado("MODIFICAR PELICULA","ADMINISTRADOR");
-    printf("\n1-Nombre de la pelicula: [%s]", aux->p.nombrePelicula);
-    printf("\n2-Nombre del director: [%s]", aux->p.director);
-    printf("\n3-Genero: [%s]", aux->p.genero);
-    printf("\n4-Pais: [%s]", aux->p.pais);
-    printf("\n5-Anio: [%i]", aux->p.anio);
-    printf("\n6-Valoracion inicial: [%i]", aux->p.valoracion);
-    printf("\n7-Clasificacion: [%i]", aux->p.pm);
-    printf("\n8-URL: [%s]", aux->p.url);
-
-    printf("\n\n0-Volver atras");
-    printf("\n\nEsperando opcion: ");
-    scanf("%i", &opcion);
-
-    if(opcion == 0)
+    if(lista==NULL)
     {
-        menuAdministrarPeliculas();
+        lista=nuevoNodo;
     }
     else
     {
-        switch(opcion)
-        {
-            case 1:
-            {
-                printf("\nIngrese el nombre de la pelicula: ");
-                fflush(stdin);
-                gets(aux->p.nombrePelicula);
-                while(existePelicula(listaP,aux->p.nombrePelicula))
-                {
-                    printf("\nIngrese el nombre de la pelicula (Error: La pelicula ya existe): ");
-                    fflush(stdin);
-                    gets(aux->p.nombrePelicula);
-                }
-                break;
-            }
-            case 2:
-            {
-                printf("\nIngrese el nombre del director: ");
-                fflush(stdin);
-                gets(aux->p.director);
-                break;
-            }
-            case 3:
-            {
-                printf("\nIngrese el genero de la pelicula: ");
-                fflush(stdin);
-                gets(aux->p.genero);
-                break;
-            }
-            case 4:
-            {
-                printf("\nIngrese el pais de la pelicula: ");
-                fflush(stdin);
-                gets(aux->p.pais);
-                break;
-            }
-            case 5:
-            {
-                printf("\nIngrese el anio en que se grabo la pelicula: ");
-                fflush(stdin);
-                scanf("%i", &aux->p.anio);
-                break;
-            }
-            case 6:
-            {
-                printf("\nIngrese la valoracion inicial de la pelicula: ");
-                fflush(stdin);
-                scanf("%i", &aux->p.valoracion);
-                break;
-            }
-            case 7:
-            {
-                //(0- si es ATP / 13: mayor de trece / 16: mayor de 16 / 18: mayor de 18;
-                printf("\nIngrese la clasificacion por edad de la pelicula\n(0- si es ATP / 13: mayor de trece / 16: mayor de 16 / 18: mayor de 18): ");
-                fflush(stdin);
-                scanf("%i", &aux->p.pm);
-                while(aux->p.pm != 0 && aux->p.pm != 13 && aux->p.pm != 16 && aux->p.pm != 18)
-                {
-                    printf("\nIngrese la clasificacion por edad de la pelicula\n(0- si es ATP / 13: mayor de trece / 16: mayor de 16 / 18: mayor de 18): ");
-                    fflush(stdin);
-                    scanf("%i", &aux->p.pm);
-                }
-                break;
-            }
-            case 8:
-            {
-                printf("\nIngrese la URL de la película:\n");
-                fflush(stdin);
-                gets(&aux->p.url);
-                while(!strlen(&aux->p.url))
-                {
-                    printf("\nIngrese la URL de la película (Error: El campo URL no puede estar vacio):\n");
-                    fflush(stdin);
-                    gets(&aux->p.url);
-                }
-                break;
-            }
-
-        }
-
-        printf("\nSe guardo la modificacion realizada.\n\n");
-        system("pause");
-        modificarPelicula(idPelicula);
-    }
-}
-
-nodoListaPelicula * insertarPeliPpio(nodoListaPelicula * listaP, nodoListaPelicula * nuevoNodo)
-{
-    if(listaP==NULL)
-    {
-        listaP=nuevoNodo;
-    }
-    else
-    {
-        nuevoNodo->sig=listaP;
-        listaP=nuevoNodo;
-    }
-    return listaP;
-}
-
-nodoListaPelicula * insertarPeliFinal(nodoListaPelicula * listaP, nodoListaPelicula * nuevoNodo)
-{
-    if(listaP==NULL)
-    {
-        listaP=nuevoNodo;
-    }
-    else
-    {
-        nodoListaPelicula * aux = buscarListaPeliUltimo(listaP);
+        nodoListaPelicula * aux = buscarListaPeliUltimo(lista);
         aux->sig=nuevoNodo;
     }
-    return listaP;
+    return lista;
 }
 
-nodoListaPelicula * insertarPeliOrden(nodoListaPelicula * listaP, nodoListaPelicula * nuevoNodo)
+nodoListaPelicula * insertarPeliOrden(nodoListaPelicula * lista, nodoListaPelicula * nuevoNodo)
 {
-    if(listaP)
+    if(lista)
     {
-        if(listaP->p.nombrePelicula > nuevoNodo->p.nombrePelicula)
+        if(lista->p.nombrePelicula > nuevoNodo->p.nombrePelicula)
         {
-            listaP = (nodoListaPelicula *)insertarPeliOrden(listaP->sig, nuevoNodo);
+            lista = (nodoListaPelicula *)insertarPeliOrden(lista->sig, nuevoNodo);
         }
         else
         {
-            nodoListaPelicula * aux = listaP->sig;
-            listaP->sig = nuevoNodo;
+            nodoListaPelicula * aux = lista->sig;
+            lista->sig = nuevoNodo;
             nuevoNodo->sig = aux;
         }
     }
     else
     {
-        listaP = nuevoNodo;
+        lista = nuevoNodo;
     }
-    return listaP;
+    return lista;
 }
 
-nodoListaPelicula * buscarListaPeliID(int idPelicula, nodoListaPelicula * listaP)
+nodoListaPelicula * buscarListaPeliID(nodoListaPelicula * lista,int idPelicula)
 {
     nodoListaPelicula * aux = NULL;
-    if(listaP)
+    if(lista)
     {
-        if(listaP->p.idPelicula == idPelicula)
+        if(lista->p.idPelicula == idPelicula)
         {
-            aux = listaP;
+            aux = lista;
         }
         else
         {
-            aux = buscarListaPeliID(idPelicula,listaP->sig);
+            aux = buscarListaPeliID(lista->sig,idPelicula);
         }
     }
     return aux;
 }
 
-nodoListaPelicula * buscarListaPeliUltimo(nodoListaPelicula * listaP)
+nodoListaPelicula * buscarListaPeliUltimo(nodoListaPelicula * lista)
 {
-    nodoListaPelicula * seg = listaP;
+    nodoListaPelicula * seg = lista;
     if(seg != NULL)
     {
         while(seg->sig != NULL)
@@ -343,51 +117,35 @@ nodoListaPelicula * buscarListaPeliUltimo(nodoListaPelicula * listaP)
             seg = seg->sig;
         }
     }
-
     return seg;
 }
 
-nodoListaPelicula * borrarPelicula(nodoListaPelicula * listaP, int id)
+nodoListaPelicula * borrarPelicula(nodoListaPelicula * lista, int id)
 {
-    if(listaP)
+    if(lista)
     {
-        if(listaP->p.idPelicula == id)
+        if(lista->p.idPelicula == id)
         {
-            nodoListaPelicula * aux = listaP;
-            listaP = listaP->sig;
+            nodoListaPelicula * aux = lista;
+            lista = lista->sig;
             free(aux);
         }
         else
         {
-            nodoListaPelicula * seg = listaP->sig;
+            nodoListaPelicula * seg = lista->sig;
             if(seg->p.idPelicula != id)
             {
-                listaP->sig = (nodoListaPelicula *)borrarPelicula(listaP->sig, id);
+                lista->sig = (nodoListaPelicula *)borrarPelicula(lista->sig, id);
             }
             else
             {
-                seg = listaP->sig;
-                listaP->sig = seg->sig;
+                seg = lista->sig;
+                lista->sig = seg->sig;
                 free(seg);
             }
         }
     }
-    return listaP;
-}
-
-void eliminarPelicula(nodoListaPelicula * listaP, int idPelicula)
-{
-    if(listaP)
-    {
-        if(listaP->p.idPelicula == idPelicula)
-        {
-            listaP->p.eliminado = !(listaP->p.eliminado);
-        }
-        else
-        {
-            eliminarPelicula(listaP->sig,idPelicula);
-        }
-    }
+    return lista;
 }
 
 nodoListaPelicula * borrarListaPeliculas(nodoListaPelicula * lista)
@@ -409,73 +167,24 @@ void mostrarPelicula(stPelicula pelicula)
     printf("%i\t%s\t%s\t\t%s\t\t%i\t%s\t%i\t%i\n",pelicula.idPelicula,pelicula.nombrePelicula,pelicula.director,pelicula.genero,pelicula.anio,pelicula.pais,pelicula.pm,pelicula.valoracion);
 }
 
-void mostrarListaPelis(nodoListaPelicula * listaP)
+void mostrarListaPelis(nodoListaPelicula * lista)
 {
-    if(listaP!=NULL)
+    if(lista!=NULL)
     {
-        mostrarPelicula(listaP->p);
-        mostrarListaPelis(listaP->sig);
+        mostrarPelicula(lista->p);
+        mostrarListaPelis(lista->sig);
     }
 }
 
-void mostrarListaPelisActivas(nodoListaPelicula * listaP)
+void mostrarListaPelisActivas(nodoListaPelicula * lista)
 {
-    if(listaP!=NULL)
+    if(lista!=NULL)
     {
-        if(listaP->p.eliminado == 0) mostrarPelicula(listaP->p);
-        mostrarListaPelisActivas(listaP->sig);
+        if(lista->p.eliminado == 0) mostrarPelicula(lista->p);
+        mostrarListaPelisActivas(lista->sig);
     }
 }
 
-nodoListaPelicula * archivoAListaPelis(const char archivo[], nodoListaPelicula * listaP, int carga)
-{
-    FILE *archi;
-    archi = fopen(archivo, "rb");
-    if(archi != NULL)
-    {
-        stPelicula aux;
-        while(fread(&aux, sizeof(stPelicula), 1, archi) > 0 && !(aux.eliminado)) //recorro el archivo de peliculas
-        {
-            nodoListaPelicula * nuevo = crearNodoListaPelicula(aux);
-            switch(carga)
-            {
-                case 1:
-                {
-                    listaP = insertarPeliPpio(arbolP,nuevo);
-                    break;
-                }
-                case 2:
-                {
-                    listaP = insertarPeliFinal(arbolP,nuevo);
-                    break;
-                }
-                case 3:
-                {
-                    listaP = insertarPeliOrden(arbolP,nuevo);
-                    break;
-                }
-            }
-        }
-        fclose(archi);
-    }
-    return listaP;
-}
-
-void listaPelisAArchivo(const char archivo[], nodoListaPelicula *listaP)
-{
-    FILE *archi;
-    archi = fopen(archivo,"wb");
-    if(archi!=NULL)
-    {
-        nodoListaPelicula * seg = listaP;
-        while(seg != NULL)
-        {
-            fwrite(&seg->p,sizeof(stPelicula),1,archi);
-            seg = seg->sig;
-        }
-        fclose(archi);
-    }
-}
 ///Modificando el struct stPelicula para así añadirle una variable llamada reproducciones hacemos que se pueda generar un promedio para calcular la valoracion.
 stPelicula calificarPelicula(stPelicula pelicula)
 {
--- listaPeliculas.h
+++ listaPeliculas.h
@@ -7,27 +7,19 @@
 #include "Menues.h"
 
 nodoListaPelicula * inicLista();
-int cantidadPeliculas(nodoListaPelicula * listaP);
-int existePelicula(nodoListaPelicula * listaP, char nombre[]);
+int cantidadPeliculas(nodoListaPelicula * lista);
+int existePelicula(nodoListaPelicula * lista, char nombre[]);
 nodoListaPelicula * crearNodoListaPelicula(stPelicula nuevo);
-stPelicula cargaPelicula();
-void altaPelicula();
-void modificarPelicula(int idPelicula);
-nodoListaPelicula * insertarPeliPpio(nodoListaPelicula * listaP, nodoListaPelicula * nuevoNodo);
-nodoListaPelicula * insertarPeliOrden(nodoListaPelicula * listaP, nodoListaPelicula * nuevoNodo);
-nodoListaPelicula * insertarPeliFinal(nodoListaPelicula * listaP, nodoListaPelicula * nuevoNodo);
-nodoListaPelicula * buscarListaPeliID(int idPelicula,nodoListaPelicula * listaP);
-nodoListaPelicula * buscarListaPeliUltimo(nodoListaPelicula * listaP);
-nodoListaPelicula * borrarPelicula(nodoListaPelicula * listaP, int id);
-void eliminarPelicula(nodoListaPelicula * listaP, int idPelicula);
+nodoListaPelicula * insertarPeliPpio(nodoListaPelicula * lista, nodoListaPelicula * nuevoNodo);
+nodoListaPelicula * insertarPeliOrden(nodoListaPelicula * lista, nodoListaPelicula * nuevoNodo);
+nodoListaPelicula * insertarPeliFinal(nodoListaPelicula * lista, nodoListaPelicula * nuevoNodo);
+nodoListaPelicula * buscarListaPeliID(nodoListaPelicula * lista, int idPelicula);
+nodoListaPelicula * buscarListaPeliUltimo(nodoListaPelicula * lista);
+nodoListaPelicula * borrarPelicula(nodoListaPelicula * lista, int id);
 nodoListaPelicula * borrarListaPeliculas(nodoListaPelicula * lista);
 void mostrarPelicula(stPelicula pelicula);
-void mostrarListaPelis(nodoListaPelicula * listaP);
-void mostrarListaPelisActivas(nodoListaPelicula * listaP);
-
+void mostrarListaPelis(nodoListaPelicula * lista);
+void mostrarListaPelisActivas(nodoListaPelicula * lista);
 stPelicula calificarPelicula(stPelicula pelicula);
-
-nodoListaPelicula * archivoAListaPelis(const char archivo[], nodoListaPelicula * listaP, int carga);
-void listaPelisAArchivo(const char archivo[], nodoListaPelicula *listaP);
 
 #endif // LISTAPELICULAS_H_INCLUDED
